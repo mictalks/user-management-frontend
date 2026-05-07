04120b16d1d16a8286ddb11ee0b09a030783f2d6
MicTalks <mictalkscorporative@gmail.com>
2026-05-07 09:57:59 -0300

    feat: initial frontend project

---
+++ pages/Home/index.jsx
@@ -0,0 +1,68 @@
+import { useEffect, useState, useRef } from 'react'
+import './style.css'
+import Trash from '../../src/assets/trash.svg'
+import api from '../../src/services/api'
+
+function Home() {
+  const [users, setUsers] = useState([])
+
+  const inputName = useRef()
+  const inputAge = useRef()
+  const inputEmail = useRef()
+
+  async function getUsers() {
+    const usersFromApi = await api.get('/usuarios')
+    setUsers(usersFromApi.data)
+  }
+
+  async function createUsers() {
+    await api.post('/usuarios', {
+      name: inputName.current.value,
+      age: inputAge.current.value,
+      email: inputEmail.current.value
+    })
+
+    getUsers()
+  }
+
+  async function deleteUsers(id) {
+    await api.delete(`/usuarios/${id}`)
+
+    getUsers()
+  }
+
+
+  useEffect(() => {
+    getUsers()
+  }, [])
+
+  return (
+    <div className="container">
+      <form>
+        <h1>Cadastro de Usuários</h1>
+
+        <input placeholder="Nome" name="username" type="text" ref={inputName} />
+        <input placeholder="Idade" name="age" type="text" ref={inputAge} />
+        <input placeholder="E-mail" name="email" type="text" ref={inputEmail} />
+
+        <button type="button" onClick={createUsers}>Cadastrar</button>
+      </form>
+
+      {users.map(user => (
+        <div key={user.id} className='card'>
+          <div>
+            <p>Nome: <span>{user.name}</span></p>
+            <p>Idade: <span>{user.age}</span></p>
+            <p>E-mail: <span>{user.email}</span></p>
+          </div>
+
+          <button onClick={() => deleteUsers(user.id)}>
+            <img src={Trash} />
+          </button>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Home
